274d8391a159f1ac7706179f3413ea06bb70bd2b
kaanyagan <kyagan@outlook.com>
2024-05-17 15:31:59 +0300

    added match service provide

--- app/src/main/java/com/kaanyagan/mislicase/data/di/NetworkModule.kt
+++ app/src/main/java/com/kaanyagan/mislicase/data/di/NetworkModule.kt
@@ -1,12 +1,14 @@
 package com.kaanyagan.mislicase.data.di
 
 import com.kaanyagan.mislicase.Constants
+import com.kaanyagan.mislicase.data.api.service.MatchService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,4 +19,9 @@ object NetworkModule {
         .baseUrl(Constants.BASE_API_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
+
+    @Provides
+    fun provideMatchService(retrofit: Retrofit): MatchService = retrofit.create(MatchService::class.java)
+
 }
