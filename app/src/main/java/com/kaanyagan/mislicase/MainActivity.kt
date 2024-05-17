d4857e61d1c2738d60e6cbedd2980f0bce1d7ea4
kaanyagan <kyagan@outlook.com>
2024-05-17 12:40:23 +0300

    initial commit

---
+++ app/src/main/java/com/kaanyagan/mislicase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kaanyagan.mislicase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
