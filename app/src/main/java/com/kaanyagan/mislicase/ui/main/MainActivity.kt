54d865cd05d5c9a63840a46f308f1b7f88be2432
kaanyagan <kyagan@outlook.com>
2024-05-17 15:53:04 +0300

    added view binding to main activity

--- app/src/main/java/com/kaanyagan/mislicase/ui/main/MainActivity.kt
+++ app/src/main/java/com/kaanyagan/mislicase/ui/main/MainActivity.kt
@@ -3,10 +3,17 @@ package com.kaanyagan.mislicase.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kaanyagan.mislicase.R
+import com.kaanyagan.mislicase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
