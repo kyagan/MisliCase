c05a71a3a0ef5a53175cc147b192786288ce505d
kaanyagan <kyagan@outlook.com>
2024-05-18 19:54:09 +0300

    created favorite model

---
+++ app/src/main/java/com/kaanyagan/mislicase/data/api/model/Favorite.kt
@@ -0,0 +1,11 @@
+package com.kaanyagan.mislicase.data.api.model
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity
+data class Favorite(
+    @PrimaryKey(autoGenerate = true) val matchId: Int = 0,
+    val i: Int
+)
